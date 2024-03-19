eaea8ab44c85678b08f2733dd8d1365426e582a0
Ashwin Kulangara Shaji <64102557+Astarioashwi@users.noreply.github.com>
2024-03-18 22:53:53 -0500

    Add files via upload

---
+++ Comprehension in python.py
@@ -0,0 +1,55 @@
+# Program for List Comprehension in python
+
+y = [1,2,3,4,5,6,7,8,9]
+z = 5
+
+l1 = [x for x in y if x %2==0]
+print(l1)
+
+l2 = [x for x in y if x %2!=0]
+print(l2)
+
+######################################################
+
+def getSmaller(l,z):
+    return [x for x in l if x < z]
+
+y = [1,2,3,4,5,6,7,8,9]
+z = 5
+print(getSmaller(y,z))
+
+######################################################
+
+def evenOdd(y):
+    even = [x for x in y if x %2==0]
+    odd = [x for x in y if x %2!=0]
+    return even, odd
+
+y = [1,2,3,4,5,6,7,8,9]
+even, odd = evenOdd(y)
+print("Odd Number: ", odd)
+print("Even Number: ", even)
+
+######################################################
+
+s = "geekforgeeks"
+l1 = [x for x in s if x in "aeious"]
+print(l1)
+
+######################################################
+
+l2 = ["geeks", "ide", "courses", "gfg"]
+l3 = [x for x in l2 if x.startswith("g")]
+print(l3)
+
+######################################################
+
+m = int(input("Enter the number: "))
+l4 = [x*2 for x in range(m)]
+print(l4)
+
+######################################################
+
+l2 = ["geeks", "ide", "courses", "gfg"]
+l3 = [x.upper() for x in l2 if x.startswith("g")]
+print(l3)
