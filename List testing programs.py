089a7941bf9c494d7daa92498c863fedb03db149
Ashwin Kulangara Shaji <64102557+Astarioashwi@users.noreply.github.com>
2024-04-12 21:59:52 -0500

    Add files via upload

---
+++ List testing programs.py
@@ -0,0 +1,41 @@
+
+l = [10, 60, 30, 40, 90, 50, 20, 70,  80]
+
+# l.append(30)
+# print(l) 
+
+# l.insert(1, 15)
+# print(l) 
+
+#print(10 in l)
+
+#print(l.count(30))
+
+#print(l.index(30))
+
+#print(l.index(30, 1, 7)) #index search between 1 to 7 indexes
+
+#l.remove(30)
+#l.remove(30)
+#print(l)
+
+#print(l.pop())
+#print(l)
+
+#print(l.pop(2))
+#print(l)
+
+#del l[1]
+#print(l)
+
+#del l[0:2]
+#print(l)
+
+#print(max(l))
+#print(min(l))
+#print(sum(l))
+#l.reverse()
+#print(l)
+
+l.sort()
+print(l)
